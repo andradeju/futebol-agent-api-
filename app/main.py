f2164adf8f83ad96012ba543898231f0531d79d8
andradeju <julliana.andrade08@gmail.com>
2025-09-23 21:13:15 -0300

    Consumindo API Futebol - listando campeonatos

--- app/main.py
+++ app/main.py
@@ -1,69 +1,74 @@
-from fastapi import FastAPI, Query
-import http.client
-import requests
+from fastapi import FastAPI
 import os
+import requests
 from dotenv import load_dotenv
 
-load_dotenv()
-app = FastAPI()
+load_dotenv() # Função pronta do pacote python-dotenv que busca um arquivo .env no projeto e carrega suas variáveis para o ambiente do sistema
+app = FastAPI()  # Cria uma "instância" = Faz como se estivesse construindo um objeto FastAPI na memória. 
 
-AGENT_API_FUTEBOL = os.getenv("AGENT_API_FUTEBOL")
-HEADERS = {
-  "X-RapidAPI-Key": AGENT_API_FUTEBOL, 
-  "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
-}
+API_KEY = os.getenv("API_KEY_FUTEBOL") # Obtém o valor da variável de ambiente "API_KEY_FUTEBOL" que foi carregada do arquivo .env
+HEADERS = {"Authorization": f"Bearer {API_KEY}"} # Cria o cabeçalho de autenticação no formato que a API externa exige
+BASE_URL = "https://api.api-futebol.com.br/v1"
 
-def get_team_id(team_name: str):
-  url = "https://api-football-v1.p.rapidapi.com"
-  params = {"search": team_name}
-  response = requests.get(url, headers=HEADERS, params=params)
-  data = response.json()
+#listar os campeonatos
+@app.get("/campeonatos")
+def listar_campeonatos():
+    url = f"{BASE_URL}/campeonatos"  # Monta a URL completa juntando base + endpoint específica
+    response = requests.get(url, headers=HEADERS) # Faz a chamada REAL para a API externa
+    if response.status_code != 200:
+        return {"error": f"Erro na requisição: {response.status_code}"}  # Retorna erro personalizado
+    
+    campeonatos = response.json() # Converte a resposta JSON em lista/dicionário Python
+    
+    # Retornando apenas os campos mais importantes
+    # Filtra apenas os dados importantes para não trafegar informação desnecessária
+    resultado = []
+    for campeonato in campeonatos:
+        resultado.append({ #.append() é um método para ADICIONAR itens a uma lista
+            "id": campeonato["campeonato_id"],
+            "nome": campeonato["nome"],
+            "nome_popular": campeonato["nome_popular"],
+            "status": campeonato["status"],
+            "tipo": campeonato["tipo"],
+        })
+    return resultado    
 
-  if data["response"]:
-    return data ["response"][0]["team"]["id"]
-  else:
-    return None
+@app.get("/campeonato/{campeonato_id}")
+def get_campeonato(campeonato_id: int):
+    url = f"{BASE_URL}/campeonatos/{campeonato_id}"
+    response = requests.get(url, headers=HEADERS)
+    return response.json()
 
-@app.get("/proximo-jogo")
-def proximo_jogo(time: str = Query(..., description="Nome do time, ex: Flamengo")):   
-  try:
-    team_id = get_team_id(time)
-    if not team_id:
-      return {"error": "Time não encontrado"}
-    url = "https://api-football-v1.p.rapidapi.com/v3/fixtures"
-    params = {"team": team_id, "next": 1}
-    response = requests.get(url, headers=HEADERS, params=params)
-    data = response.json()  
+# Rota para pegar os times da fase atual de um campeonato
+# @app.get("/campeonato/{campeonato_id}/times")
+# def get_teams(campeonato_id: int):
+#     # Pega fase atual
+#     campeonato = requests.get(f"{BASE_URL}/campeonatos/{campeonato_id}", headers=HEADERS).json()
+#     fase_id = campeonato["fase_atual"]["fase_id"]
 
-    if not data["response"]:
-        return {"message": f"Não encontrei próximos jogos para {time}"}
+#     # Pega times da fase
+#     url_fase = f"{BASE_URL}/campeonatos/{campeonato_id}/fases/{fase_id}"
+#     data_fase = requests.get(url_fase, headers=HEADERS).json()
+#     times = []
 
-    jogo = data["response"][0]
-    adversario = (
-      jogo["teams"]["home"]["name"]
-      if jogo["teams"]["away"]["name"].lower() == time.lower()
-      else jogo["teams"]["away"]["name"]
-    )
+#     for grupo in data_fase.get("grupos", []):
+#         for time in grupo.get("times", []):
+#             times.append({
+#                 "id": time["time"]["id"],
+#                 "nome": time["time"]["nome"],
+#                 "sigla": time["time"]["sigla"],
+#                 "escudo": time["time"]["escudo"]
+#             })
 
-    return {
-      "time": time,
-      "adversario": adversario,
-      "data": jogo["fixture"]["date"],
-      "estadio": jogo["fixture"]["venue"]["name"],
-      "transmissao": jogo["fixture"].get("broadcast", "Ainda não divulgado")
-    }
+#     # Se não tiver grupos, alguns campeonatos retornam direto
+#     if not times and "times" in data_fase:
+#         for time in data_fase["times"]:
+#             times.append({
+#                 "id": time["time"]["id"],
+#                 "nome": time["time"]["nome"],
+#                 "sigla": time["time"]["sigla"],
+#                 "escudo": time["time"]["escudo"]
+#             })
 
-  except Exception as e:
-    return {"error": f"Erro ao buscar jogo: {str(e)}"}   
+#     return times
 
-@app.get("/countries")
-def get_countries():
-    conn = http.client.HTTPSConnection("v3.football.api-sports.io")
-    headers = {
-        'x-rapidapi-host': "v3.football.api-sports.io",
-        'x-rapidapi-key': AGENT_API_FUTEBOL
-    }
-    conn.request("GET", "/countries", headers=headers)
-    res = conn.getresponse()
-    data = res.read()
-    return data.decode("utf-8")     
